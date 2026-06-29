b7c2a8091699293c6aa7eae26e6cf28f55c0db1f
maureen-mueni <muenimaureen61@gmail.com>
2026-06-29 15:46:38 +0300

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:eventify/main.dart';
-
-void main() {
-  testWidgets('App load smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const EventifyApp());
-    expect(find.text('Eventify'), findsWidgets);
-  });
-}
